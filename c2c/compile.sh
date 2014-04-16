4a5d1775759ea730c98e1753fedea943e5ad71a6
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2014-04-16 19:28:00 +0200

    compile script: added compiling to .o

--- c2c/compile.sh
+++ c2c/compile.sh
@@ -5,3 +5,8 @@ echo "compiling $1"
 rm -f a.out /tmp/output.s
 llc -march=x86 $1 -o /tmp/output.s
 clang /tmp/output.s -o a.out
+
+# to compile to .o
+#llc -march=x86 -cppgen=module foo.ll
+#clang -c foo.s -o foo.o
+
