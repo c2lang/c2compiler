44fefac73b2b0aea2ecaa36d3c10450f2f2ddd37
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2014-04-28 09:09:12 +0200

    Update Legal header for year 2014

--- tools/xml2dot/XmlReader.h
+++ tools/xml2dot/XmlReader.h
@@ -1,4 +1,4 @@
-/* Copyright 2011,2012 Bas van den Berg
+/* Copyright 2013,2014 Bas van den Berg
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
