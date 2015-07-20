fb3ecdc0a896106244dc1bbed0aec273fbed2324
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2015-07-20 17:03:57 +0200

    copyright: changed 2013,2014,2015 into 2013-2015 for clarity

--- c2c/AST/Decl.h
+++ c2c/AST/Decl.h
@@ -1,4 +1,4 @@
-/* Copyright 2013,2014,2015 Bas van den Berg
+/* Copyright 2013-2015 Bas van den Berg
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
