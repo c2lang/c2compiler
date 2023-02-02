f3cc11d27869f501e41f361570685bd07f23cf87
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2023-02-02 18:01:14 +0100

    env: added env.sh

---
+++ env.sh
@@ -0,0 +1,6 @@
+echo 'setting C2 environment'
+export C2_LIBDIR=$HOME/code/c2compiler/c2libs
+export C2_PLUGINDIR=$HOME/c2c/plugins
+export CC=clang
+export CXX=clang++
+#export PATH=$HOME/llvm-11/bin:$PATH
