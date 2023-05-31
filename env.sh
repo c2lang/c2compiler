29cdd9c707b3706fcc126e954f481d7fe105fe35
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2023-05-31 18:47:43 +0200

    Component: add ExternalStatic and ExternalDynamic kinds

--- env.sh
+++ env.sh
@@ -3,3 +3,4 @@ export C2_LIBDIR=$PWD/../c2_libs
 export C2_PLUGINDIR=$HOME/c2c/plugins
 export CC=clang
 export CXX=clang++
+complete -W '-a -A -b -c -C -d -f -h -m -q -Q -r -s -S -t -T -v --check --create --fast --help --help-recipe --showlibs --targets --test' c2c
