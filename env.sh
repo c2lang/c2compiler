9c0cba6a710aad275a1a5a8406609d1ea8023102
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2023-06-26 23:17:33 +0200

    plugins: first plugin_mgr, working AST

--- env.sh
+++ env.sh
@@ -3,4 +3,4 @@ export C2_LIBDIR=$PWD/../c2_libs
 export C2_PLUGINDIR=$HOME/c2c/plugins
 export CC=clang
 export CXX=clang++
-#complete -W '-a -A -b -c -C -d -f -h -m -q -Q -r -s -S -t -T -v --check --create --fast --help --help-recipe --showlibs --targets --test' c2c
+#complete -W '-a -A -b -c -C -d -f -h -m -q -Q -r -s -S -t -T -v --check --create --fast --help --help-recipe --showlibs --showplugins --targets --test' c2c
