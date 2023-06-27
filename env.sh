82d32155b92dda9c7948b66eab025116417f4119
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2023-06-27 19:38:12 +0200

    plugin: add paths from build_file

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 echo 'setting C2 environment'
 export C2_LIBDIR=$PWD/../c2_libs
-export C2_PLUGINDIR=$HOME/c2c/plugins
+export C2_PLUGINDIR=$PWD
 export CC=clang
 export CXX=clang++
 #complete -W '-a -A -b -c -C -d -f -h -m -q -Q -r -s -S -t -T -v --check --create --fast --help --help-recipe --showlibs --showplugins --targets --test' c2c
