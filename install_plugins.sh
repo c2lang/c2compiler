42e7a3f2ed2971e489a6b39f4779a1fff053cf20
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2023-09-26 21:19:07 +0200

    tooling: also strip plugins during installation

--- install_plugins.sh
+++ install_plugins.sh
@@ -7,3 +7,4 @@ cp output/load_file/libload_file.so $C2_PLUGINDIR
 cp output/refs_generator/librefs_generator.so $C2_PLUGINDIR
 cp output/shell_cmd/libshell_cmd.so $C2_PLUGINDIR
 cp output/unit_test/libunit_test.so $C2_PLUGINDIR
+strip $C2_PLUGINDIR/*.so
