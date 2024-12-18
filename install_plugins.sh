4e97c12026830daa58b0e5d9630b8601d8cf4ee6
Charlie Gordon <github@chqrlie.org>
2024-12-18 01:37:35 +0100

    Partial port to Darwin arm64
    bootstrap:
    * keep intermediary target **c2c_bootstrap2**
    * use dynamic compiler detection for gcc/clang and target architecture
    * adjust warning flags for gcc/clang
    * define `fallthrough` according to compiler version
    * use `#ifdef` to select architecture dependent definitions
    * fix `target_info_arch_names` name as `arm64` instead of `arm_64`
    * add **globals** to generate C library constants and structures
    
    common/target_info.c2:
    * add `Info.define` for C library source preprocessing
    * use `arm64` instead of `arm_64` in `arch_names`
    
    generator/:
    * generate `fallthrough` pseudo-statements instead of
      `__attribute__((fallthrough))` for portability
    * generate `Makefile` with clang specific warning flags
    * adjust dynamic library link command
    
    compiler:
    * add target architecture define for C library source preprocessing
    * select library extension based on target architecture
    
    tools/tester/test_db.c2:
    * reduce read buffer size to 128k in `Db.testFile`, causes stack overflow
    * make `Db.printIssues` return error status
    
    tools/tester/tester.c2:
    * use `snprintf` instead of `sprintf`
    * control verbosity with `-t` and `-v`
    * control threading with `-n` and `-j[#]` (with optional number of threads)
    * more flexible options parser: accept multiple options before or after file/dir
    * fix crash bug on empty filename arg, do not strip `/` from root directory

--- install_plugins.sh
+++ install_plugins.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
+if [ $(uname -s) = 'Darwin' ] ; then
+   LIB_EXT='.dylib'
+else
+   LIB_EXT='.so'
+fi
 set -e
 mkdir -p $C2_PLUGINDIR
-cp output/deps_generator/libdeps_generator.so $C2_PLUGINDIR
-cp output/git_version/libgit_version.so $C2_PLUGINDIR
-cp output/load_file/libload_file.so $C2_PLUGINDIR
-cp output/refs_generator/librefs_generator.so $C2_PLUGINDIR
-cp output/shell_cmd/libshell_cmd.so $C2_PLUGINDIR
-cp output/unit_test/libunit_test.so $C2_PLUGINDIR
-strip $C2_PLUGINDIR/*.so
+cp output/deps_generator/libdeps_generator$LIB_EXT $C2_PLUGINDIR
+cp output/git_version/libgit_version$LIB_EXT $C2_PLUGINDIR
+cp output/load_file/libload_file$LIB_EXT $C2_PLUGINDIR
+cp output/refs_generator/librefs_generator$LIB_EXT $C2_PLUGINDIR
+cp output/shell_cmd/libshell_cmd$LIB_EXT $C2_PLUGINDIR
+cp output/unit_test/libunit_test$LIB_EXT $C2_PLUGINDIR
+if [ $(uname -s) != 'Darwin' ] ; then
+   strip $C2_PLUGINDIR/*$LIB_EXT
+fi
