e876ab8993c6b75f4905392c8d1d765b0013dc27
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2024-12-23 08:25:41 +0100

    darwin: partial merge of Charly Gordon's Darwin patch

--- env.sh
+++ env.sh
@@ -2,5 +2,4 @@ echo 'setting C2 environment'
 export C2_LIBDIR=~/c2_libs
 export C2_PLUGINDIR=~/c2_plugins
 export CC=clang
-export CXX=clang++
 #complete -W '-a -A -b -c -C -d -f -h -m -q -Q -r -s -S -t -T -v --check --create --fast --help --help-recipe --showlibs --showplugins --targets --test' c2c
