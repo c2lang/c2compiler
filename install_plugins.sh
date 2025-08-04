64de53550d2cb1bd27d5b940bd9abaf3073b3535
Bas van den Berg <b.van.den.berg.nl@gmail.com>
2025-08-04 06:21:59 +0200

    embed: allow embedding files
    * refactor duplicate attribute checking
    * removed obsolete load_file plugin
    * works on text files and binary files (both 0 terminated atm)
    
    syntax: const u8[] Data1 @(embed="filename.txt");
    
    rules:
    - the variable must be a global.
    - the variable may be public/not
    - the type must be const u8[]
    - there may not be an initializer

--- install_plugins.sh
+++ install_plugins.sh
@@ -17,7 +17,6 @@ set -e
 mkdir -p $C2_PLUGINDIR
 cp output/deps_generator/libdeps_generator$LIB_EXT $C2_PLUGINDIR
 cp output/git_version/libgit_version$LIB_EXT $C2_PLUGINDIR
-cp output/load_file/libload_file$LIB_EXT $C2_PLUGINDIR
 cp output/refs_generator/librefs_generator$LIB_EXT $C2_PLUGINDIR
 cp output/shell_cmd/libshell_cmd$LIB_EXT $C2_PLUGINDIR
 cp output/unit_test/libunit_test$LIB_EXT $C2_PLUGINDIR
